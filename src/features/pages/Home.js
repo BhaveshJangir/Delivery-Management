6481f92aa8a4f6719dc10fbf29d11a94c72df268
Harsh <harshjaiswal40383@gmail.com>
2023-09-24 17:51:47 +0530

    initial

---
+++ src/features/pages/Home.js
@@ -0,0 +1,13 @@
+import Navbar from "../navbar/Navbar";
+import ProductList from "../product-list/ProductList";
+function Home() {
+    return ( 
+        <Navbar>
+            <ProductList>
+
+            </ProductList>
+        </Navbar>
+     );
+}
+
+export default Home;
