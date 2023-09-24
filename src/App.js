0d55fd5a7b7fce829cc15ca7bec29436aa0032a9
Harsh <harshjaiswal40383@gmail.com>
2023-09-24 19:48:05 +0530

    part 2: adding routing(login&signup)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from "react";
 import "./App.css";
-//harsh
-// import Home from "./features/pages/Home";
-// import LoginPage from "./features/pages/LoginPage";
+
+import Home from "./features/pages/Home";
+import LoginPage from "./features/pages/LoginPage";
 import SignupPage from "./features/pages/SignupPage";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./features/auth/components/Login";
+import Signup from "./features/auth/components/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home></Home>)
+  },
+  
+  {
+    path: "/login",
+    element: (<LoginPage></LoginPage>)
+  },
+  {
+    path: "/signup",
+    element: (<SignupPage></SignupPage>)
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <SignupPage/>
+      <RouterProvider router={router} />
+      
     </div>
   );
 }
